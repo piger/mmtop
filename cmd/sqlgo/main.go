02756a75f025758f5dd6d30725edfa67a53cde6c
Daniel Kertesz <dkertesz@zendesk.com>
2017-08-03 01:05:50 +0100

    command line flags

--- cmd/sqlgo/main.go
+++ cmd/sqlgo/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/piger/sqlgo"
 	"os"
 )
 
+var configFilename = flag.String("config", "config.ini", "Configuration filename")
+
 func main() {
-	configs, err := sqlgo.ReadConfig("config.ini")
+	flag.Parse()
+
+	configs, err := sqlgo.ReadConfig(*configFilename)
 	if err != nil {
 		fmt.Printf("Error in configuration: %s\n", err)
 		os.Exit(1)
-	}
-
-	for _, config := range configs {
-		fmt.Printf("mysql://%s:%s@%s:%d\n", config.Username, config.Password, config.Address, config.Port)
 	}
 
 	sqlgo.RunClient(configs)
